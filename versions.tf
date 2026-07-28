5fe672524c87d7cde5a896a90c85d540b4f007d9
Matt Cockayne <matthew.cockayne@gmail.com>
2026-07-28 06:20:47 +0000

    chore: drop unused tls provider (fleeting uses EC2 Instance Connect)

--- versions.tf
+++ versions.tf
@@ -6,10 +6,5 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
-    # Manager↔worker SSH key generation for the fleeting connector.
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~> 4.0"
-    }
   }
 }
